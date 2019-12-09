9fb0f2617fa9396a76ec0a62de9b14313a1212c0
Menilek <menilek25@hotmail.co.uk>
2019-12-09 20:15:43 +0000

    Changed MongoDB env var due to a change in DB provider

--- server.js
+++ server.js
@@ -9,22 +9,23 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
-const prodDB = process.env.MONGODB_URI;
-const devDB = process.env.mongoURI;
+const prodDB = process.env.MongoDB_URI;
 
 mongoose
-    .connect(prodDB || devDB, {useNewUrlParser: true})
+    .connect(prodDB, {useNewUrlParser: true})
     .then(() => console.log('MongoDB connected!'))
     .catch(err => console.log(err));
 
+app.use('/api/entries', require('./routes/api/entries'));
+
 if(process.env.NODE_ENV === 'production'){
+    
     app.use(express.static('client/build'));
+    
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
-
-app.use('/api/entries', require('./routes/api/entries'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
